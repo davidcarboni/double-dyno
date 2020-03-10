169f226b3c072917aa875b40f0eb1c5ec61a149b
David Carboni <david@carboni.io>
2020-03-10 10:36:46 +0000

    Now calls the API.

--- web/index.js
+++ web/index.js
@@ -1,6 +1,7 @@
 // https://medium.com/@adnanrahic/hello-world-app-with-node-js-and-express-c1eb7cfa8a30
 
 var express = require('express');
+var request = require('request');
 var app = express();
 
 // So we can exit the Docker container with Ctrl-C
@@ -10,9 +11,16 @@ process.on('SIGINT', function() {
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+  request('http://api:3000/data', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(JSON.parse(body))
+    } else {
+      res.send("Error")
+    }
+  })
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Web app listening on port 3000!');
 });
