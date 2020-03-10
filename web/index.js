a7566bc233dc0877e240222b25955024e6ec3a35
David Carboni <david@carboni.io>
2020-03-10 10:22:04 +0000

    Added exit on Ctrl-C

--- web/index.js
+++ web/index.js
@@ -1,8 +1,18 @@
+// https://medium.com/@adnanrahic/hello-world-app-with-node-js-and-express-c1eb7cfa8a30
+
 var express = require('express');
 var app = express();
+
+// So we can exit the Docker container with Ctrl-C
+// https://github.com/nodejs/node/issues/4182
+process.on('SIGINT', function() {
+  process.exit();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
